b297d6522b0bdd1e7128f62ca7a3154283d87ad2
DESKTOP-SPNOEQI <jjescuderv@eafit.edu.co>
2021-04-02 02:14:08 -0500

    Ejemplo de GUI y requirements.txt

--- gui.py
+++ gui.py
@@ -1,1 +1,21 @@
 # https://realpython.com/pysimplegui-python/#getting-started-with-pysimplegui
+import PySimpleGUI as gui
+
+gui.theme('DarkAmber')
+
+layout = [  [gui.Text('Some text on Row 1')],
+            [gui.Text('Enter something on Row 2'), gui.InputText()],
+            [gui.Button('Test'), gui.Cancel()]
+         ]
+
+
+window = gui.Window('Graphic User Interface', layout)
+
+while True:             
+    event, values = window.read()
+    if event in (gui.WIN_CLOSED, 'Cancel'):
+        break
+    if event == 'Test':
+        print('test')
+
+window.close()
