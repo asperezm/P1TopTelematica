c3fbc0478a8e3d5ca451f38f82e3e9f5a6d017cb
asperezm <47333056+asperezm@users.noreply.github.com>
2021-04-02 16:08:58 -0500

    app and .bat

---
+++ app_module.py
@@ -0,0 +1,47 @@
+import socket
+import os
+import subprocess
+import random
+import time
+from datetime import datetime
+from datetime import date
+
+HEADER = 1024
+PORT = 5050
+FORMAT = 'utf-8'
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER,PORT)
+
+app = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+app.connect(ADDR)
+
+def start():
+    msg="App"
+    current_time = datetime.now().strftime("%H:%M:%S")
+    current_date = datetime.today().strftime("%d/%m/%Y")
+    app.send(bytes(msg,FORMAT))
+    connection = True
+    while connection:
+        msg = app.recv(HEADER).decode(FORMAT)
+        
+        command = msg.split(',')
+        
+        if(command[3]!="status:Error"):
+            if(command[-1]=="Open"):
+                subprocess.call("app.bat")
+                num = random.randrange(1,4)
+                if(num==1 or num==2):
+                    status="processed"
+                elif(num == 3):
+                    status="busy"
+                else:
+                    status="Error"
+
+                msg="cmd:send,src:App,dst:log,status:"+status+",msg:\"log: " + current_time + " "+ current_date+ ",Open"
+                app.send(bytes(msg,FORMAT))
+
+        else:
+            subprocess.call("close.bat")
+            msg="cmd:send,src:App,dst:log,status:Error,msg:\"log: " + current_time + " "+ current_date+ ",Error close app"
+            app.send(bytes(msg,FORMAT))
+
