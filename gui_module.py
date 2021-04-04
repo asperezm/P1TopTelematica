55c8ee4c92aae42c11688f4c5a7aac1da206a12b
jsacevedoc <jsacevedoc@eafit.edu.co>
2021-04-03 20:02:47 -0500

    Panel de procesos

--- gui_module.py
+++ gui_module.py
@@ -1,10 +1,11 @@
 # https://realpython.com/pysimplegui-python/#getting-started-with-pysimplegui
 import PySimpleGUI as sg
 import socket
-import datetime
 import os
 from PySimpleGUI.PySimpleGUI import VerticalSeparator
 import time
+from datetime import datetime
+from datetime import date
 
 #Connection
 HEADER = 1024
@@ -18,12 +19,6 @@ gui.connect(ADDR)
 
 
 def start():
-
-    current_time = "67328"
-    current_date = "3768232"
-
-
-
 
     msg="Gui"
     gui.send(msg.encode(FORMAT))
@@ -39,12 +34,15 @@ def start():
     sg.ChangeLookAndFeel('LightGreen')      # set the overall color scheme
     column1=[   [sg.Text('Orgullo OS lleva corriendo::', font='Any 12'),sg.Text('', size=(30,1), key='_DATE_')],
                 [sg.Text('Gestionar módulo aplicaciones', size=(50,2), justification='center')],
-                [sg.Button('Calc', font=('Any 15'), button_color=('white','#3eb548')), sg.Button('Close all', font=('Any 15'), button_color=('white', '#3f56d1')) ],
+                [sg.Button('Calc', font=('Any 15'), button_color=('white','#3eb548')),sg.Button('Close', font=('Any 15'), button_color=('white', '#3f56d1')), sg.Button('Close all', font=('Any 15'), button_color=('white', '#e04646'))],
                 [sg.Text('', size=(50,1), justification='center')],
                 [sg.HorizontalSeparator(pad=None)],
-                [sg.Text('Log de transacciones', size=(50,2))],
-                [sg.Output(size=(50,16))],
-                [sg.Text('', size=(50,6))],
+                [sg.Text('Procesos', size=(50,1))],
+                [sg.Listbox(values=(["total","ome","gonorrea"]), size=(50,10), enable_events=True, key='-LISTOTA-')],
+                [sg.HorizontalSeparator(pad=None)],
+                [sg.Text('Log de transacciones', size=(50,1))],
+                [sg.Output(size=(50,10))],
+                [sg.Text('', size=(50,1))],
                 [sg.HorizontalSeparator(pad=None)],
                 [sg.Button('Apagar sistema', button_color=('white', '#e04646'), key='Exit')]
             ]
@@ -76,14 +74,14 @@ def start():
     # create the "Window"
     window = sg.Window('Orgullo OS', layout=layout, default_element_size=(12,1),font='Any 12')
 
-    start_time = datetime.datetime.now()
+    start_time = datetime.now()
 
     
     #  The "Event loop" where all events are read and processed (button clicks, etc)
     while True:
+        current_time = datetime.now().strftime("%H:%M:%S")
+        current_date = datetime.today().strftime("%d/%m/%Y")
 
-        current_time = "67328"
-        current_date = "3768232"
         event, values = window.Read(timeout=10)     # read with a timeout of 10 ms
         if event != sg.TIMEOUT_KEY:                 # if got a real event, print the info
             pass
@@ -125,7 +123,7 @@ def start():
 
         
         # Output the "uptime" statistic to a text field in the window
-        window.Element('_DATE_').Update(str(datetime.datetime.now()-start_time))
+        window.Element('_DATE_').Update(str(datetime.now()-start_time))
 
 
     # Exiting the program
